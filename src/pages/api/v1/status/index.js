fb5403d440b722f1c0caf455477655e7c4ba7061
João Vitor de Sousa <joaovitordesousapereira@gmail.com>
2025-03-27 19:15:25 +0000

    feature: implementação de criar bd local

--- src/pages/api/v1/status/index.js
+++ src/pages/api/v1/status/index.js
@@ -1,5 +1,5 @@
-function Home() {
-  return <h1>Te amo manitos</h1>;
-}
-
-export default Home;
+function status(request, response) {
+    response.status(200).json({ chave: "são acima da média" });
+  }
+  
+  export default status;
