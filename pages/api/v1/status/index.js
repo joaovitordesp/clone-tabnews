8853ed6497403c5385a0690252517f8f4210a8ed
João Vitor de Sousa <joaovitordesousapereira@gmail.com>
2025-04-02 15:24:05 +0000

    ajuste nas conexçoes

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,27 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-    const result = await database.query('select 1+1 as sum');
-    console.log(result.rows)
-    response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString(); 
+
+  const databaseVersion = await database.query("SHOW server_version")
+  const databaseVersionValue = databaseVersion.rows[0].server_version
+
+  const databaseMaxConnection = await database.query("SHOW max_connections")
+  const databaseMaxConnectionValue = databaseMaxConnection.rows[0].max_connections
+
+  const databaseOpenConnection = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname='local_db';")
+  const databaseOpenConnectionValue = databaseOpenConnection.rows[0].count
+
+  response.status(200).json({
+      updated_at: updatedAt,
+      dependencies:{
+        database: {
+          version: databaseVersionValue,
+          max_connections: parseInt(databaseMaxConnectionValue),
+          open_connections: databaseOpenConnectionValue
+        }
+      }
+    });
   }
   
   export default status;
