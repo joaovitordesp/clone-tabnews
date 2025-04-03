6d6b00ffa54b62cf87bef41a606bca3605753f98
João Vitor de Sousa <joaovitordesousapereira@gmail.com>
2025-04-03 12:17:48 +0000

    config database for production

--- infra/database.js
+++ infra/database.js
@@ -6,7 +6,8 @@ async function query(queryObject){
         port:process.env.POSTGRES_PORT,
         user:process.env.POSTGRES_USER,
         database:process.env.POSTGRES_DATABASE,
-        password:process.env.POSTGRES_PASSWORD
+        password:process.env.POSTGRES_PASSWORD,
+        ssl:true
     });
     
     try {
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,7 +2,7 @@ test("GET to /api/v1/status should be return 200", async()=>{
     const response = await fetch("http://localhost:3000/api/v1/status")
     expect(response.status).toBe(200)
 
-    const responseBody = await response.json()
+    const responseBody = await response.json('Conseguiu conectar')
 
     const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
     expect(responseBody.updated_at).toEqual(parseUpdatedAt);
