76c50060bbea522bdb3e2c3378793e96a89e234a
João Vitor de Sousa <joaovitordesousapereira@gmail.com>
2025-04-08 15:33:37 +0000

    feature: include migrations and tests about it

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ git add - sobe alterações para a staging area.
 git commit - realiza novos commits.
 git commit --amend - substitui o commit anterior por um novo, mas aproveita as alterações dele.
 git diff - calcula a diferença entre as versões/alterações dos arquivos.
+
+
+->migrations
+"migration:create":"node-pg-migrate --migrations-dir infra/migrations create",
+    "migration:up": "node-pg-migrate -m infra/migrations --envPath .env.development up"
+
+    --migrations-dir é a mesma coisa que -m
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,13 +1,18 @@
-test("GET to /api/v1/status should be return 200", async()=>{
-    const response = await fetch("http://localhost:3000/api/v1/status")
+import database from "infra/database.js"
+
+beforeAll(cleanDatabase)
+
+async function cleanDatabase() {
+    await database.query("drop schema public cascade; create schema public;")
+}
+
+test("GET to /api/v1/migrations should be return 200", async()=>{
+    const response = await fetch("http://localhost:3000/api/v1/migrations")
     expect(response.status).toBe(200)
 
-    const responseBody = await response.json('Conseguiu conectar')
+    const responseBody = await response.json();
+    console.log(responseBody)
 
-    const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
-    expect(responseBody.updated_at).toEqual(parseUpdatedAt);
-
-    expect(responseBody.dependencies.database.version).toEqual("16.0")
-    expect(responseBody.dependencies.database.max_connections).toEqual(100)
-    expect(responseBody.dependencies.database.open_connections).toEqual(1)
+    expect(Array.isArray(responseBody)).toBe(true)
+    expect(responseBody.length).toBeGreaterThan(0)
 })
