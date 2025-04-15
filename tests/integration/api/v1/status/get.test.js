8b041537d523ca6ddd121a5e092d6f1adf886da9
João Vitor de Sousa <joaovitordesousapereira@gmail.com>
2025-04-15 15:37:33 +0000

    makes > clone-tabnews@1.0.0 test > npm run services:up && npm run wait-for-postgres && concurrently -n next,jest --hide next -k -s command-jest 'sleep 5;next dev' 'jest --runInBand'
    > clone-tabnews@1.0.0 services:up
    > docker compose -f infra/compose.yaml up -d
    
    > clone-tabnews@1.0.0 wait-for-postgres
    > node infra/scripts/wait-for-postgres.js
    
    [32m✔ Postgres está pronto e aceitando conexões![0m
    [jest] FAIL tests/integration/api/v1/status/get.test.js (6.167 s)
    [jest]   ● GET to /api/v1/status should be return 200
    [jest]
    [jest]     ReferenceError: database is not defined
    [jest]
    [jest]       3 | beforeAll(async () =>{
    [jest]       4 |   await orchestrator.waitForAllServices();
    [jest]     > 5 |   await database.query("drop schema public cascade; create schema public;");
    [jest]         |   ^
    [jest]       6 | })
    [jest]       7 |
    [jest]       8 | test("GET to /api/v1/status should be return 200", async () => {
    [jest]
    [jest]       at Object.<anonymous> (tests/integration/api/v1/status/get.test.js:5:3)
    [jest]
    [jest] PASS tests/integration/api/v1/migrations/get.test.js
    [jest] PASS tests/integration/api/v1/status/post.test.js
    [jest]
    [jest] Test Suites: 1 failed, 2 passed, 3 total
    [jest] Tests:       1 failed, 2 passed, 3 total
    [jest] Snapshots:   0 total
    [jest] Time:        6.64 s, estimated 13 s
    [jest] Ran all test suites.
    [jest] jest --runInBand exited with code 1
    --> Sending SIGTERM to other processes.. async test

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,18 +1,21 @@
+import orchestrator from "tests/orchestrator";
 import database from "infra/database.js";
 
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
+beforeAll(async () =>{
+  await orchestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
-}
+})
 
-test("GET to /api/v1/migrations should be return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
+test("GET to /api/v1/status should be return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  console.log(responseBody);
+  const responseBody = await response.json("Conseguiu conectar");
 
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.open_connections).toEqual(1);
 });
