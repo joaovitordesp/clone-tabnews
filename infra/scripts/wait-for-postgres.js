a881c0027bc298311882f810574e3151672badad
João Vitor de Sousa <joaovitordesousapereira@gmail.com>
2025-04-28 20:17:07 +0000

    lint: fix use lint fix

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,36 +1,36 @@
-const {exec} = require("node:child_process")
+const { exec } = require("node:child_process");
 
 let intervaloBolinhas;
 let indiceBolinhas = 0;
 const bolinhas = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
 
 function loadingMassinha() {
-    intervaloBolinhas = setInterval(() => {
-      const frame = bolinhas[indiceBolinhas++ % bolinhas.length];
-      process.stdout.write(
-        `\r\x1b[33m${frame} Aguardando Postgres aceitar conexões...\x1b[0m `,
-      );
-    }, 100);
-  }
-  
-  function stopLoading() {
-    clearInterval(intervaloBolinhas);
+  intervaloBolinhas = setInterval(() => {
+    const frame = bolinhas[indiceBolinhas++ % bolinhas.length];
     process.stdout.write(
-      `\r\x1b[32m✔ Postgres está pronto e aceitando conexões!\x1b[0m\n`,
+      `\r\x1b[33m${frame} Aguardando Postgres aceitar conexões...\x1b[0m `,
     );
-  }
+  }, 100);
+}
 
-function checkPostgres(){
-    exec("docker exec postgres-dev pg_isready --host localhost", handleReturn)
+function stopLoading() {
+  clearInterval(intervaloBolinhas);
+  process.stdout.write(
+    `\r\x1b[32m✔ Postgres está pronto e aceitando conexões!\x1b[0m\n`,
+  );
+}
 
-    function handleReturn(error, stdout){
-        if(stdout.search("accepting connections") === -1){
-            setTimeout(checkPostgres, 500);
-            return;
-        }
+function checkPostgres() {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
 
-        stopLoading();
+  function handleReturn(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      setTimeout(checkPostgres, 500);
+      return;
     }
+
+    stopLoading();
+  }
 }
 
 loadingMassinha();
--- pages/construction.js
+++ pages/construction.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import styles from './Construction.module.css';
+import React from "react";
+import styles from "./Construction.module.css";
 
 const ConstructionPage = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Estamos em Construção!</h1>
       <p className={styles.message}>
-        Desculpe pela inconveniência. Estamos trabalhando para trazer algo incrível para você.
+        Desculpe pela inconveniência. Estamos trabalhando para trazer algo
+        incrível para você.
       </p>
       <div className={styles.loader}></div>
       <p className={styles.footer}>Volte em breve!</p>
@@ -14,4 +15,4 @@ const ConstructionPage = () => {
   );
 };
 
-export default ConstructionPage; 
+export default ConstructionPage;
